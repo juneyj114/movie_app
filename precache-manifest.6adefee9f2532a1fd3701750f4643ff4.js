f72207b221f2f7b779e6fc325a4d8db1eefb535d
juney <juneyj114@gmail.com>
2019-10-20 01:43:48 +0900

    Updates

---
+++ precache-manifest.6adefee9f2532a1fd3701750f4643ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a9f320c50e452915ab866f9140c6e4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2365083579c280db6bab",
+    "url": "/movie_app/static/css/main.756e0f15.chunk.css"
+  },
+  {
+    "revision": "06e3e7e1964695cad340",
+    "url": "/movie_app/static/js/2.77edc3c2.chunk.js"
+  },
+  {
+    "revision": "2365083579c280db6bab",
+    "url": "/movie_app/static/js/main.e9de2f21.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
